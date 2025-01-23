5432fd83c60e205e032bcd39b2ec9f4b62826cd9
Icenowy Zheng <uwu@icenowy.me>
2025-01-23 18:30:28 +0800

    add support for DRI3 1.2
    DRI3 1.2 is needed for mainline Mesa Zink w/o LIBGL_KOPPER_DRI2=1
    environment variable.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- etnadrm/etnaviv_dri3.c
+++ etnadrm/etnaviv_dri3.c
@@ -18,6 +18,7 @@
 
 /* drm includes */
 #include <xf86drm.h>
+#include <drm_fourcc.h>
 
 #include "fb.h"
 #include "gcstruct.h"
@@ -62,6 +63,25 @@ static PixmapPtr etnaviv_dri3_pixmap_from_fd(ScreenPtr pScreen, int fd,
 					  stride, depth, bpp);
 }
 
+static PixmapPtr etnaviv_dri3_pixmap_from_fds(ScreenPtr pScreen, CARD8 num_fds,
+	const int *fds, CARD16 width, CARD16 height, const CARD32 *strides,
+	const CARD32 *offsets, CARD8 depth, CARD8 bpp, CARD64 modifier)
+{
+	if (num_fds != 1)
+		return NULL;
+	if (*offsets != 0)
+		return NULL;
+	switch (modifier) {
+	case DRM_FORMAT_MOD_LINEAR:
+	case DRM_FORMAT_MOD_INVALID:
+		break;
+	default:
+		return NULL;
+	}
+
+	return etnaviv_dri3_pixmap_from_fd(pScreen, *fds, width, height, (CARD16) *strides, depth, bpp);
+}
+
 static int etnaviv_dri3_fd_from_pixmap(ScreenPtr pScreen, PixmapPtr pixmap,
 	CARD16 *stride, CARD32 *size)
 {
@@ -70,7 +90,7 @@ static int etnaviv_dri3_fd_from_pixmap(ScreenPtr pScreen, PixmapPtr pixmap,
 
 	/* Only support pixmaps backed by an etnadrm bo */
 	if (!vPix || !vPix->etna_bo)
-		return BadMatch;
+		return -1;
 
 	*stride = pixmap->devKind;
 	*size = etna_bo_size(vPix->etna_bo);
@@ -78,11 +98,54 @@ static int etnaviv_dri3_fd_from_pixmap(ScreenPtr pScreen, PixmapPtr pixmap,
 	return etna_bo_to_dmabuf(etnaviv->conn, vPix->etna_bo);
 }
 
+static int etnaviv_dri3_fds_from_pixmap(ScreenPtr pScreen, PixmapPtr pixmap,
+	int *fds, CARD32 *strides, CARD32 *offsets, CARD64 *modifier)
+{
+	CARD32 size;
+	CARD16 stride;
+	int fd = etnaviv_dri3_fd_from_pixmap(pScreen, pixmap, &stride, &size);
+
+	if (fd < 0)
+		return 0;
+
+	*fds = 1;
+	*offsets = 0;
+	*strides = stride;
+	*modifier = DRM_FORMAT_MOD_INVALID;
+	return 1;
+}
+
+static int etnaviv_dri3_get_formats(ScreenPtr screen, CARD32 *num_formats,
+	CARD32 **formats)
+{
+	*num_formats = 0;
+	return TRUE;
+}
+
+static int etnaviv_dri3_get_modifiers(ScreenPtr screen, uint32_t format,
+	uint32_t *num_modifiers, uint64_t **modifiers)
+{
+	*num_modifiers = 0;
+	return TRUE;
+}
+
+static int etnaviv_dri3_get_drawable_modifiers(DrawablePtr drawable,
+	uint32_t format, uint32_t *num_modifiers, uint64_t **modifiers)
+{
+	*num_modifiers = 0;
+	return TRUE;
+}
+
 static dri3_screen_info_rec etnaviv_dri3_info = {
-	.version = 0,
+	.version = 2,
 	.open = etnaviv_dri3_open,
 	.pixmap_from_fd = etnaviv_dri3_pixmap_from_fd,
 	.fd_from_pixmap = etnaviv_dri3_fd_from_pixmap,
+	.pixmap_from_fds = etnaviv_dri3_pixmap_from_fds,
+	.fds_from_pixmap = etnaviv_dri3_fds_from_pixmap,
+	.get_formats = etnaviv_dri3_get_formats,
+	.get_modifiers = etnaviv_dri3_get_modifiers,
+	.get_drawable_modifiers = etnaviv_dri3_get_drawable_modifiers,
 };
 
 Bool etnaviv_dri3_ScreenInit(ScreenPtr pScreen)
